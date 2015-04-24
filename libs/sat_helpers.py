f464263e226caa09d32926f9b0124ff06fc7c5e6
Yoram Hekma <hekma.yoram@gmail.com>
2015-04-24 09:26:26 +0200

    Initial commit

---
+++ libs/sat_helpers.py
@@ -0,0 +1,13 @@
+import re
+import xmlrpclib
+
+
+def getserver():
+    with open('/etc/sysconfig/rhn/up2date', 'r') as up2date_handle:
+        up2date_config = up2date_handle.read()
+        return "%s/rpc/api" % re.match('.*\n\s*serverURL=(.*?)XMLRPC', up2date_config).group(0)
+
+
+def create_connection(url, username, password, verbose=0):
+    server_con = xmlrpclib.ServerProxy(uri=url, verbose=verbose)
+    return server_con.auth.login(username, password)
---
+++ satelans.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+import sys
+import os
+import ConfigParser
+import json
+import shlex
+from libs import sat_helpers
+
+
+def get_config(conf_file):
+    conf_obj = ConfigParser.RawConfigParser()
+    conf_obj.read(conf_file)
+    return {
+        'url': conf_obj.get('main', 'url'),
+        'username': conf_obj.get('main', 'username'),
+        'password': conf_obj.get('main', 'password'),
+        'groups': shlex.split(conf_obj.get('main', 'groups')),
+    }
+
+
+def dump_json_list(sat_connection, groups):
+    result = {'_meta': {'hostvars': dict()}}
+
+    for group in groups:
+        hosts = [i['name'] for i in
+                 sat_connection.satServer.systemgroup.listSystemsMinimal(sat_connection.satAuth, group)]
+        result[group] = {
+            'hosts': hosts,
+        }
+
+        for host in hosts:
+            result['_meta']['hostvars'][host] = {}
+    print json.dumps(result)
+
+
+if __name__ == "__main__":
+    self_dir = os.path.dirname(os.path.abspath(__file__))
+    config = get_config("%s/satelans.ini")
+    connection = sat_helpers.create_connection(
+        url=config['url'],
+        username=config['username'],
+        password=config['password'],
+    )
+
+    if sys.argv[1] == '--host':
+        print {}
+        sys.exit()
+    if sys.argv[1] == '--list':
+        dump_json_list(connection, config['groups'])
+        sys.exit()
