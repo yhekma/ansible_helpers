ea9f8ae18af59e49215f401071a60499f96190d4
Yoram Hekma <hekma.yoram@gmail.com>
2015-04-29 14:32:45 +0200

    now supporting multiple groups

--- Satellite/gen_inv.py
+++ Satellite/gen_inv.py
@@ -3,6 +3,7 @@
 import sys
 import os
 import json
+import xmlrpclib
 self_dir = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, os.path.join(self_dir, '..'))
 from ansible_helpers_libs.satellite_helpers import get_json, get_config, create_connection
@@ -12,26 +13,27 @@ except ImportError:
     import ansible_helpers_libs.argparse_local as argparse
 
 
-def main(c_auth, conn, group, section):
-    hosts_dict = json.loads(get_json(conn, c_auth, [group]))
-    hosts = hosts_dict['hosts']
-    print '[%s]\n%s' % (section, '\n'.join(hosts))
+parser = argparse.ArgumentParser()
+parser.add_argument('-c', '--config',
+                    help='Location of the config file, defaults to global variable SATELANS_CONFIG.',
+                    default=os.getenv('SATELANS_CONFIG'))
+parser.add_argument('-g', '--group', help='Which Satellite group(s) to query. Multiples can be comma-seperated.')
+parser.add_argument('-s', '--section', help='Name of the Ansible section to generate.')
+args = parser.parse_args()
+groups = args.group(',')
 
+config = get_config(args.config)
+auth, connection = create_connection(
+    url=config['url'],
+    username=config['username'],
+    password=config['password'],
+)
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-c', '--config',
-                        help='Location of the config file, defaults to global variable SATELANS_CONFIG.',
-                        default=os.getenv('SATELANS_CONFIG'))
-    parser.add_argument('-g', '--group', help='Which Satellite group to query.')
-    parser.add_argument('-s', '--section', help='Name of the Ansible section to generate, defaults to groupname.')
-    args = parser.parse_args()
-
-    config = get_config(args.config)
-    auth, connection = create_connection(
-        url=config['url'],
-        username=config['username'],
-        password=config['password'],
-    )
-
-
+print '[%s]' % args.section
+for group in groups:
+    try:
+        hosts_dict = json.loads(get_json(connection, auth, group))
+        hosts = hosts_dict[group]['hosts']
+        print '%s' % ('\n'.join(hosts))
+    except xmlrpclib.Fault:
+        pass
